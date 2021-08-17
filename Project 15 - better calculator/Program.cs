c56e1429d87c6f76da32ca6e4efd52ca46043ed5
Krupa Ankith NK <aalisi.ankith@gmail.com>
2021-08-17 16:44:13 +0530

    Add project files.

---
+++ Project 15 - better calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Project_15___better_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter an integer: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+
+            else if (op == "^")
+                if (num2 != 3)
+                {
+                    Console.WriteLine(num1 * num1);
+                }
+                else if (op == "^") num2 = 3;
+            {
+                Console.WriteLine(num1 * num1 * num1);
+            }
+
+            else 
+
+
+
+            
+            {
+                Console.WriteLine(num1 * num1 * num1 * num1);
+            }
+               
+            
+
+            Console.ReadLine();
+        }
+    }
+}
